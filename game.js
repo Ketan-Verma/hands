9ee61157e8a25e5514a690d544e79d63ec6e6973
Ketan Verma <ketancation@gmail.com>
2023-03-13 22:56:48 +0530

    comrem

--- game.js
+++ game.js
@@ -1,31 +1,24 @@
 init()
-// Show the loader
 document.getElementById('loader').style.display = 'block';
 document.getElementById('webcam-container').style.visibility="hidden" //style.display = 'none';
 
-// Hide the loader after a delay (in this case, 3 seconds)
 setTimeout(function() {
     document.getElementById('loader').style.display = 'none';
     document.getElementById('webcam-container').style.visibility="visible" //style.display = 'none';
 }, 5000);
 
-// Define the game choices as constants
 const ROCK = 'rock';
 const PAPER = 'paper';
 const SCISSORS = 'scissors';
 
-// Define the game logic
 function playRound(playerSelection, computerSelection) {
-  // Make both inputs lowercase to standardize comparison
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
 
-  // Check for a tie
   if (playerSelection === computerSelection) {
     return 'Tie!';
   }
 
-  // Check for player wins
   if (
     (playerSelection === ROCK && computerSelection === SCISSORS) ||
     (playerSelection === PAPER && computerSelection === ROCK) ||
@@ -34,11 +27,9 @@ function playRound(playerSelection, computerSelection) {
     return `You win! ${playerSelection} beats ${computerSelection}.`;
   }
 
-  // If it's not a tie and the player hasn't won, then the computer wins
   return `You lose! ${computerSelection} beats ${playerSelection}.`;
 }
 
-// Define a function to get the computer's choice
 function computerPlay() {
   
     const choices = [ROCK, PAPER, SCISSORS];
@@ -49,9 +40,7 @@ function computerPlay() {
     return compChoice ;
 }
 
-// Define a function to handle a game round
 function game() {
-//   let playerSelection = prompt('Choose rock, paper, or scissors:');
   let computerSelection = computerPlay();
   const imgs = document.getElementById("imgs")
   imgs.style.display ="flex"
@@ -61,19 +50,13 @@ function game() {
   console.log(playRound(choice.toUpperCase(), computerSelection));
 }
 
-// start event
 
-// Start the game
 function countdownAndCelebrate() {
     const elementsWithSameClass = document.getElementsByClassName("choice");
           for (let i = 0; i < elementsWithSameClass.length; i++) {
             elementsWithSameClass[i].classList.remove("clrbg");
           }
-    // Get the element where the countdown and celebration message will be displayed
     const countdownElement = document.getElementById('countdown');
-    // const imgs = document.getElementById("imgs")
-    // imgs.style.display ="none"
-    // Start the countdown
     let count = 3;
     countdownElement.innerText = count;
     const countdownInterval = setInterval(() => {
